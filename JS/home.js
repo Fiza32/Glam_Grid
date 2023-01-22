d87203fd86a592d91368bf2990628f7aadf744a6
Fiza32 <mdfaizan878@gmail.com>
2023-01-22 21:46:18 +0530

    Home Page JS

---
+++ JS/home.js
@@ -0,0 +1,24 @@
+
+// Dropdown JS
+function dropDown1() {
+    let content = document.querySelector(".drop-elem1");
+
+    content.addEventListener("mouseover", function () {
+        let menu = document.querySelector(".dd-menu1");
+        menu.style.display = "block";
+    });
+
+    let menu = document.querySelector(".dd-menu1");
+    menu.addEventListener("mouseover", function () {
+        menu.style.display = "block";
+        menu.addEventListener("mouseout", function () {
+            menu.style.display = "none";
+        });
+    });
+
+    content.addEventListener("mouseout", function () {
+        let menu = document.querySelector(".dd-menu1");
+        menu.style.display = "none";
+    });
+}
+dropDown1();
